bfaae7640ad6745ed915d87428dff8b0b6bb170d
sai <sai@digital-lync.com>
2023-04-20 12:50:28 +0530

    20apr

--- src/Containers/Product.js
+++ src/Containers/Product.js
@@ -1,5 +1,6 @@
 
 import { useState, useEffect } from 'react';
+import React from 'react';
 import axios from 'axios';
 function Product() {
     const [product, setProduct] = useState({
@@ -11,7 +12,12 @@ function Product() {
         "seller": ""
     })
 
+    var formRef = React.useRef();
+
+
+
     const [products, setProducts] = useState([]);
+
     const updateState = (event) => {
         var name = event.target.name;
         var value = event.target.value;
@@ -19,22 +25,50 @@ function Product() {
     }
 
     useEffect(() => {
-
-        axios.get("http://localhost:9091/product/list").then(
-            response => {
-                console.log("response", response)
-            },
-            error => {
-                console.log(error);
-            }
-        )
+        getProducts();
     }, [])
     const addProduct = (event) => {
         event.preventDefault();
         console.log(product);
         axios.post("http://localhost:9091/product/add", product).then(
             response => {
-                console.log("response", response)
+                console.log("response", response);
+                formRef.current.reset();
+                getProducts();
+
+            },
+            error => {
+                console.log(error);
+            }
+        )
+    }
+    const displayProducts = () => {
+        let list = products.map(product => {
+            return <tr key={product.name}> <td>{product.id} </td> <td><img style={{ width: "60px", height: "64px" }} src={product.imageUrl}></img></td> <td>{product.name}</td> <td>{product.price}</td> <td><button onClick={() => deleteProduct(product.id)}>Delete</button></td><td><button onClick={() => editProduct(product.id)}>Edit</button></td> </tr>
+        })
+        return list;
+    }
+
+    const editProduct = (productId) => {
+
+    }
+
+    const deleteProduct = (prodId) => {
+        axios.delete("http://localhost:9091/product/delete", { data: { "id": prodId } }).then(
+            response => {
+                console.log(response);
+                getProducts();
+            },
+            error => {
+                console.log(error);
+            }
+        )
+    }
+    const getProducts = () => {
+        axios.get("http://localhost:9091/product/list").then(
+            response => {
+                console.log("response.data", response.data);
+                setProducts(response.data);
             },
             error => {
                 console.log(error);
@@ -44,7 +78,7 @@ function Product() {
     return (
         <div>
             <h1 style={{ textAlign: 'center' }}>Add Product</h1>
-            <form style={{ textAlign: 'center' }}>
+            <form ref={formRef} style={{ textAlign: 'center' }}>
                 <input onChange={updateState} type="text" name="name" placeholder="product name"></input> <br></br><br></br>
                 <input onChange={updateState} type="number" name="price" placeholder="price"></input><br></br><br></br>
                 <input onChange={updateState} type="text" name="description" placeholder="description here"></input><br></br><br></br>
@@ -53,6 +87,26 @@ function Product() {
                 <input onChange={updateState} type="text" name="seller" placeholder="seller here"></input><br></br><br></br>
                 <button onClick={addProduct}>Add Product</button>
             </form>
+
+            <hr></hr>
+
+            <table border="1" style={{ width: "100%" }}>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Image</th>
+                        <th>Name</th>
+
+                        <th>Price</th>
+                        <th colSpan="2">Operation</th> </tr>
+                </thead>
+
+                <tbody style={{ textAlign: "center" }}>
+                    {displayProducts()}
+                </tbody>
+
+            </table>
+
         </div>
     )
 
